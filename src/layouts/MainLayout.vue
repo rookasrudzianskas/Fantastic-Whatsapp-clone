aefc421bbb98911da36e45c0bb45973483f8d4fa
Rokas <rookas.rudzenskas@gmail.com>
2021-04-22 14:30:24 +0300

    Rokas

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title class="absolute-center">
+          {{ title }}
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    title() {
+      let currentPath = this.$route.fullPath
+      if(currentPath == '/') return 'SmackChat'
+      else if(currentPath == '/chat') return 'Chat'
+      else  if(currentPath == '/auth') return 'Login'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ src/pages/PageAuth.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page>
+    <h5>Auth page</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
