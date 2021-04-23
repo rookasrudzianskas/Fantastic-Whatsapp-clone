5a9d2b87126670e987b36d73e8bbffec2e1196ea
Rokas <rookas.rudzenskas@gmail.com>
2021-04-23 13:11:49 +0300

    Rokas

--- src/store/store.js
+++ src/store/store.js
@@ -13,6 +13,14 @@ const actions = {
     firebaseAuth.createUserWithEmailAndPassword(payload.email, payload.password)
       .then(response => {
         console.log(response)
+        let userId = firebaseAuth.currentUser.uid
+        firebaseDb.ref('users/' + userId).set({
+
+          name: payload.name,
+          email: payload.email,
+          online: true
+
+        })
       })
       .catch(error => {
         console.log(error.message)
