4f1d67cc2b7061ff06350307dcf88ead8c0da213
Rokas <rookas.rudzenskas@gmail.com>
2021-04-26 12:12:03 +0300

    Rokas

--- src/pages/PageUsers.vue
+++ src/pages/PageUsers.vue
@@ -1,55 +1,45 @@
 <template>
   <q-page class="flex q-pa-md">
-    <q-list class="full-width" separator>
-      <q-item v-for="user in users" :key="user.id" clickable v-ripple to="/chat">
-        <q-item-section avatar>
-          <q-avatar color="primary" text-color="white">
-            {{ user.name.charAt(0) }}
-          </q-avatar>
-        </q-item-section>
+  	<q-list
+  		class="full-width"
+  		separator>
+  	  <q-item
+  	  	v-for="(user, key) in users"
+  	  	:key="key"
+  	  	to="/chat"
+  	  	clickable
+  	  	v-ripple>
+  	    <q-item-section avatar>
+  	      <q-avatar color="primary" text-color="white">
+  	        {{ user.name.charAt(0) }}
+  	      </q-avatar>
+  	    </q-item-section>
 
-        <q-item-section>
-          <q-item-label>{{ user.name }}</q-item-label>
-          <q-item-label caption lines="1">{{ user.email }}</q-item-label>
-        </q-item-section>
+  	    <q-item-section>
+  	      <q-item-label>{{ user.name }}</q-item-label>
+  	    </q-item-section>
 
-        <q-item-section side>
-          <q-badge :color="user.online ? 'light-green-5' : 'grey-4'">
-            {{ user.online ? 'online 🤜' : 'offline 💁‍♂️'}}
+  	    <q-item-section side>
+  	      <q-badge
+  	      	:color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online ? 'Online' : 'Offline' }}
           </q-badge>
-        </q-item-section>
-      </q-item>
-    </q-list>
+  	    </q-item-section>
+  	  </q-item>
 
+  	</q-list>
   </q-page>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
 
-  export default {
-    data() {
-      return {
-          users: [{
-          id: 1,
-          name: 'Rokas Jedrzej',
-            online: true,
-        }, {
-          id: 2,
-          name: 'Danny Alessandrini',
-            online: false,
-        }, {
-          id: 3,
-          name: 'Lina Wicklen',
-            online: false,
-        }, {
-          id: 4,
-          name: 'Rusne Simtakoje',
-            online: false,
-        }]
-      }
+	export default {
+    computed: {
+      ...mapGetters('store', ['users'])
     }
-}
+	}
 </script>
-<style>
 
+<style>
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,96 +1,125 @@
-import { firebaseAuth, firebaseDb } from "boot/firebase";
+import Vue from 'vue'
+import { firebaseAuth, firebaseDb } from 'boot/firebase'
 
 const state = {
-  userDetails: {
-
-  }
+	userDetails: {},
+	users: {}
 }
-
 const mutations = {
-  setUserDetails(state, payload) {
-   state.userDetails = payload
-  }
+	setUserDetails(state, payload) {
+		state.userDetails = payload
+	},
+	addUser(state, payload) {
+		Vue.set(state.users, payload.userId, payload.userDetails)
+	},
+	updateUser(state, payload) {
+		Object.assign(state.users[payload.userId], payload.userDetails)
+	}
 }
-
 const actions = {
-  registerUser({}, payload) {
-    firebaseAuth.createUserWithEmailAndPassword(payload.email, payload.password)
-      .then(response => {
-        console.log(response)
-        let userId = firebaseAuth.currentUser.uid
-        firebaseDb.ref('users/' + userId).set({
+	registerUser({}, payload) {
+		firebaseAuth.createUserWithEmailAndPassword(payload.email, payload.password)
+			.then(response => {
+				console.log(response)
+				let userId = firebaseAuth.currentUser.uid
+				firebaseDb.ref('users/' + userId).set({
+					name: payload.name,
+					email: payload.email,
+					online: true
+				})
+			})
+			.catch(error => {
+				console.log(error.message)
+			})
+	},
+	loginUser({}, payload) {
+		firebaseAuth.signInWithEmailAndPassword(payload.email, payload.password)
+			.then(response => {
+				console.log(response)
+			})
+			.catch(error => {
+				console.log(error.message)
+			})		
+	},
+	logoutUser() {
+		firebaseAuth.signOut()
+	},
+	handleAuthStateChanged({ commit, dispatch, state }) {
+		firebaseAuth.onAuthStateChanged(user => {
+		  if (user) {
+		    // User is logged in.
+		    let userId = firebaseAuth.currentUser.uid
+		    firebaseDb.ref('users/' + userId).once('value', snapshot => {
+		    	let userDetails = snapshot.val()
+		    	commit('setUserDetails', {
+		    		name: userDetails.name,
+		    		email: userDetails.email,
+		    		userId: userId
+		    	})
+		    })
+		    dispatch('firebaseUpdateUser', {
+		    	userId: userId,
+		    	updates: {
+		    		online: true
+		    	}
+		    })
+		    dispatch('firebaseGetUsers')
+		    this.$router.push('/')
+		  }
+		  else {
+		  	// User is logged out.
+		  	dispatch('firebaseUpdateUser', {
+		  		userId: state.userDetails.userId,
+		  		updates: {
+		  			online: false
+		  		}
+		  	})
+		  	commit('setUserDetails', {})
+		  	this.$router.replace('/auth')
+		  }
+		})
+	},
 
-          name: payload.name,
-          email: payload.email,
-          online: true
-
-        })
-      })
-      .catch(error => {
-        console.log(error.message)
-      })
-  },
-  loginUser({}, payload) {
-    firebaseAuth.signInWithEmailAndPassword(payload.email, payload.password)
-      .then(response => {
-        console.log(response)
-      })
-      .catch(error => {
-        console.log(error.message)
-      })
-  },
-  logoutUser() {
-    firebaseAuth.signOut()
-  },
-  handleAuthStateChanged({ commit, dispatch, state }) {
-    firebaseAuth.onAuthStateChanged(user => {
-      if (user) {
-        // use ris longed in
-        let userId = firebaseAuth.currentUser.uid
-        firebaseDb.ref('users/' + userId).once('value', snapshot => {
-          let userDetails = snapshot.val()
-          commit('setUserDetails', {
-            name: userDetails.name,
-            email: userDetails.email,
-            userId: userId
-          })
-        })
-        dispatch('firebaseUpdateUser', {
-          userId: userId,
-          updates: {
-            online: true
-          }
-        })
-        this.$router.push('/')
-
-      } else {
-        // use is logged out
-
-        dispatch('firebaseUpdateUser', {
-          userId: state.userDetails.userId,
-          updates: {
-            online: false
-          }
-        })
-        commit('setUserDetails', {})
-        this.$router.replace('/auth')
-      }
-    })
-  },
-  firebaseUpdateUser({}, payload) {
-    // console.log(payload)
-    firebaseDb.ref('users/' + payload.userId).update(payload.updates)
-  }
+	firebaseUpdateUser({}, payload) {
+		if (payload.userId) {
+			firebaseDb.ref('users/' + payload.userId).update(payload.updates)
+		}
+	},
+	firebaseGetUsers({ commit }) {
+		firebaseDb.ref('users').on('child_added', snapshot => {
+			let userDetails = snapshot.val()
+			let userId = snapshot.key
+			commit('addUser', {
+				userId,
+				userDetails
+			})
+		})
+		firebaseDb.ref('users').on('child_changed', snapshot => {
+			let userDetails = snapshot.val()
+			let userId = snapshot.key
+			commit('updateUser', {
+				userId,
+				userDetails
+			})
+		})
+	}
 }
-
 const getters = {
-
+	users: state => {
+		let usersFiltered = {}
+		Object.keys(state.users).forEach(key => {
+			if (key !== state.userDetails.userId) {
+				usersFiltered[key] = state.users[key]
+			}
+		})
+		return usersFiltered
+	}
 }
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions,
-  getters
+	namespaced: true,
+	state,
+	mutations,
+	actions,
+	getters
 }
