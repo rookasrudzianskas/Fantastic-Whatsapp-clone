e33804d3f9af74a33d02f4de6d050e4494d40bd2
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 12:07:50 +0300

    Rokas

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -1,13 +1,17 @@
 <template>
-  <q-page class="flex column">
+  <q-page
+    ref="pageChat"
+    class="flex column">
 
     <q-banner class=" text-center bg-grey-4" v-if="!otherUserDetails.online">
       {{ otherUserDetails.name }} is offline
     </q-banner>
 
-    <div class="q-pa-md column col justify-end">
-      <q-chat-message v-for="message in messages"
-        :key="message.text"
+    <div
+      :class="{'invisible' : !showMessages}"
+      class="q-pa-md column col justify-end">
+      <q-chat-message v-for="(message, key) in messages"
+        :key="key"
         :name="message.from == 'me' ? userDetails.name : otherUserDetails.name"
         :text="[message.text]"
         :sent="message.from == 'me' ? true : false"
@@ -38,7 +42,7 @@ export default {
   data() {
     return {
       newMessage: '',
-
+      showMessages: false,
     }
 
   },
@@ -49,7 +53,6 @@ export default {
   methods: {
     ...mapActions('store', ['firebaseGetMessages', 'firebaseStopGettingMessages', 'firebaseSendMessage']),
     sendMessage() {
-      console.log('👽', 'clicked')
       this.firebaseSendMessage({
         message: {
           text: this.newMessage,
@@ -57,6 +60,23 @@ export default {
         },
         otherUserId: this.$route.params.otherUserId
       })
+    },
+    scrollToBottom() {
+      let pageChat = this.$refs.pageChat.$el
+      setTimeout(() => {
+        window.scrollTo(0, pageChat.scrollHeight)
+      }, 20)
+
+    }
+  },
+  watch: {
+    messages: function (val) {
+      if(Object.keys(val).length) {
+        this.scrollToBottom()
+        setTimeout(() => {
+          this.showMessages = true
+        }, 200)
+      }
     }
   },
   mounted() {
--- src/store/store.js
+++ src/store/store.js
@@ -29,7 +29,6 @@ const actions = {
 	registerUser({}, payload) {
 		firebaseAuth.createUserWithEmailAndPassword(payload.email, payload.password)
 			.then(response => {
-				console.log(response)
 				let userId = firebaseAuth.currentUser.uid
 				firebaseDb.ref('users/' + userId).set({
 					name: payload.name,
